c5e0c3d0a7321fc6e2216bdbc19a6be921e81e0f
VitorFabrisDev <vitor2fabris@hotmail.com>
2023-10-06 13:20:58 -0300

    Ajuste da versão do SpringBoot para a 2.7.17

---
+++ src/main/java/com/tcs_senac/ruralfacil/RuralfacilApplication.java
@@ -0,0 +1,15 @@
+package com.tcs_senac.ruralfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.Entity;
+
+@SpringBootApplication
+public class RuralfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralfacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs_senac/ruralfacil/RuralfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs_senac.ruralfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
