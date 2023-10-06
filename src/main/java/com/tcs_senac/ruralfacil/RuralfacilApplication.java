ef9cd4e5c0ec15e87c989f630281c75b85d32afb
VitorFabrisDev <vitor2fabris@hotmail.com>
2023-10-06 13:32:21 -0300

    Teste Commit

--- src/main/java/com/tcs_senac/ruralfacil/RuralfacilApplication.java
+++ src/main/java/com/tcs_senac/ruralfacil/RuralfacilApplication.java
@@ -3,13 +3,13 @@ package com.tcs_senac.ruralfacil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.Entity;
 
 @SpringBootApplication
 public class RuralfacilApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RuralfacilApplication.class, args);
+		System.out.println("teste");
 	}
 
 }
