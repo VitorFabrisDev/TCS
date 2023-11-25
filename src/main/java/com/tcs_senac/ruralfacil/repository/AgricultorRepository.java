dd82c8c0f4cf804cc01df87599bfbbe10cee6256
VitorFabrisDev <vitor2fabris@hotmail.com>
2023-11-24 23:55:17 -0300

    Ajuste no cadastro de pessoa. Retirar e-mail e tipoendereco

--- src/main/java/com/tcs_senac/ruralfacil/repository/AgricultorRepository.java
+++ src/main/java/com/tcs_senac/ruralfacil/repository/AgricultorRepository.java
@@ -9,13 +9,11 @@ import java.util.Optional;
 @Repository
 public interface AgricultorRepository extends JpaRepository<Agricultor, Long> {
 
-    boolean existsByEmail(String email);
 
     boolean existsByCpf(String cpf);
 
     boolean existsByCpfAndIdNot(String cpf, Long id);
 
-    Optional<Agricultor> findByEmail(String email);
 
     Optional<Agricultor> findByCpf(String cpf);
 
